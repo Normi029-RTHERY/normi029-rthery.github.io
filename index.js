0f987f4d970b3f230580e660e349e533e84fc7d0
Normi029-RTHERY <rom.t95@free.fr>
2022-12-05 17:04:05 +0100

    euuh

--- index.js
+++ index.js
@@ -18,6 +18,6 @@
 }
 function animateOpening() {
     console.log('hey')
-    document.querySelector("main").setAttribute('style', 'animation: openingAnimation 200ms ease-in');
+    document.querySelector("main").setAttribute('style', 'animation: openingAnimation 300ms ease-in');
     setTimeout(function () { document.querySelector("main").setAttribute('style', 'clip-path: circle(100% at 50% 50%)'); }, 190)
 }
