2a5ce660335cec09245aacce6cd47b895d25e5db
RTHERY-Normi029 <rom.t95@free.fr>
2024-11-21 14:54:38 +0100

    New animated background using p5.js, updated styling

---
+++ apps/sketch.js
@@ -0,0 +1,47 @@
+const scl = 35;
+const inc = 0.07;
+let zoff = 0;
+let cols, rows;
+
+let vectors = [];
+const vMag = 0.5;
+
+function setup() {
+  const canvas = createCanvas(window.innerWidth, window.innerHeight);
+  cols = canvas.width / scl;
+  rows = canvas.height / scl;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(30, 30, 30, 50);
+
+  let xoff = 0;
+  for (let x = 0; x < cols + scl; x++) {
+    let yoff = 0;
+    for (let y = 0; y < rows + scl; y++) {
+      const i = x + y * cols;
+      const a = noise(xoff, yoff, zoff) * 2 * TWO_PI;
+      const v = p5.Vector.fromAngle(a);
+      v.setMag(vMag);
+      vectors[i] = v;
+
+      // show vectors
+      stroke(255);
+      strokeWeight(0.5);
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      line(0, 0, scl, 0);
+      pop();
+
+      yoff += inc;
+    }
+    xoff += inc;
+  }
+  zoff += 0.005;
+  // print(frameRate());
+}
---
+++ temp/scripts/index.js
@@ -0,0 +1,23 @@
+{
+    let i = Math.floor(Math.random() * 3)
+    const e = document.documentElement;
+    switch (i) {
+        case 0:
+            e.style.setProperty('--wlcm-grad-l', 'rgb(0,140,230)');
+            e.style.setProperty('--wlcm-grad-r', 'rgb(140,0,230)');
+            break;
+        case 1:
+            e.style.setProperty('--wlcm-grad-l', 'rgb(0,230,140)');
+            e.style.setProperty('--wlcm-grad-r', 'rgb(140,230,0)');
+            break;
+        case 2:
+            e.style.setProperty('--wlcm-grad-l', 'rgb(230,140,0)');
+            e.style.setProperty('--wlcm-grad-r', 'rgb(230,0,140)');
+            break;
+    }
+}
+function animateOpening() {
+    console.log('hey')
+    document.querySelector("main").setAttribute('style', 'animation: openingAnimation 300ms ease-in');
+    setTimeout(function () { document.querySelector("main").setAttribute('style', 'clip-path: circle(100% at 50% 50%)'); }, 190)
+}
